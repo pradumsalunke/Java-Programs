7ba194ce6c652ff329af5f3f78a8d695f1c4a656
Pradum Salunke <118037582+pradumsalunke@users.noreply.github.com>
2024-03-20 00:10:32 +0530

    Add files via upload

---
+++ Triangle123.java
@@ -0,0 +1,14 @@
+class Triangle123
+{
+public static void main(String[] args) {
+
+	for(i=1; i<=5; i++)
+	{
+		for(j=1; j<=i; j++)
+			num=1;
+		{
+			System.out.print(num++ +" ");
+		}
+	}
+}	
+}
---
+++ TrianglePatternReverse.java
@@ -0,0 +1,15 @@
+class TrianglePatternReverse
+{
+	public static void main(String[] args) {
+	  int num=1;
+	  for(int i=1; i<=5; i++)
+	  {
+	  	for(int j=1; j<=i; j++)
+	  	{
+	  		System.out.print(num++ +" ");
+	  		num+=1;
+	  	}
+	  	System.out.println();
+	  }
+	}
+}
